4ee2d75405f5d62e43b73486ccb875fc9839acba
ArnaudGrondin <grondin828@gmail.com>
2024-12-05 16:38:43 +0100

    Ajout de la serialisation de tache

--- task.py
+++ task.py
@@ -1,5 +1,5 @@
 import time
-
+import json
 import numpy as np
 
 
@@ -19,3 +19,29 @@ class Task:
         start = time.perf_counter()
         self.x = np.linalg.solve(self.a, self.b)
         self.time = time.perf_counter() - start
+
+    def to_json(self) -> str:
+        d = {
+            "identifier": self.identifier,
+            "a": self.a.tolist(),
+            "b": self.b.tolist(),
+            "size": self.size,
+            "x": self.x.tolist(),
+            "time": self.time,
+        }
+        return json.dumps(d)
+
+    @staticmethod
+    def from_json(text: str) -> "Task":
+        d = json.loads(text)
+        task = Task(d["identifier"], d["size"])
+        task.a = np.array(d["a"])
+        task.a = np.array(d["b"])
+        task.x = np.array(d["x"])
+        task.size = d["size"]
+        task.time = d["time"]
+        return task
+
+    def __eq__(self, other: "Task") -> bool:
+        same = self.identifier == other.identifier and self.size == other.size
+        return same
--- test_task.py
+++ test_task.py
@@ -1,7 +1,7 @@
 import unittest
 from task import Task
 import numpy as np
-# TODO VERIFIER QUE LE TEST ECHOUE # uv run python unittest
+# pour  verifier que le test échoue $ uv run python unittest
 
 
 class TestTask(unittest.TestCase):
@@ -9,6 +9,13 @@ class TestTask(unittest.TestCase):
         t = Task()
         t.work()
         np.testing.assert_allclose(t.a @ t.x, t.b)
+
+    def test_serialisation(self):
+        a = Task()
+        txt = a.to_json()
+
+        b = Task.from_json(txt)
+        np.testing.assert_equal(a, b)
 
 
 if __name__ == "__main__":
